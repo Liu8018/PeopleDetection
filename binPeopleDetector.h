2e46d798d636a3ced67f7d627aa7baf47a69e0b9
liu <liuyiguang8018@gmail.com>
2019-02-08 12:08:27 +0800

    ...

--- binPeopleDetector.h
+++ binPeopleDetector.h
@@ -12,7 +12,8 @@ class binPeopleDetector
 {
 private:
     cv::HOGDescriptor m_peopleHog;
-    cv::CascadeClassifier faceDetector;
+    cv::CascadeClassifier m_faceDetector;
+    cv::CascadeClassifier m_upperBodyDetector;
     
     double m_hitThreshold;
     cv::Size m_winStride;
@@ -31,7 +32,8 @@ public:
         m_scale = 1.1;
         m_groupThreshold = 10;
         
-        faceDetector.load("../PeopleDetection/xmls/lbpcascade_frontalface_improved.xml");
+        m_faceDetector.load("../PeopleDetection/xmls/haarcascade_frontalface_default.xml");
+        m_upperBodyDetector.load("../PeopleDetection/xmls/haarcascade_upperbody.xml");
     }
     
     void detectPeople(const cv::Mat &frame1, const cv::Mat &frame2, 
@@ -44,8 +46,15 @@ public:
     void detectFace(const cv::Mat &frame1, const cv::Mat &frame2, 
                     std::vector<cv::Rect> &faceRects1, std::vector<cv::Rect> &faceRects2)
     {
-        faceDetector.detectMultiScale(frame1,faceRects1);
-        faceDetector.detectMultiScale(frame2,faceRects2);
+        m_faceDetector.detectMultiScale(frame1,faceRects1);
+        m_faceDetector.detectMultiScale(frame2,faceRects2);
+    }
+    
+    void detectUpperBody(const cv::Mat &frame1, const cv::Mat &frame2, 
+                    std::vector<cv::Rect> &ubRects1, std::vector<cv::Rect> &ubRects2)
+    {
+        m_upperBodyDetector.detectMultiScale(frame1,ubRects1);
+        m_upperBodyDetector.detectMultiScale(frame2,ubRects2);
     }
 
 };
